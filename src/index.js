9cde4789e441e10f2991a6a1cf6e862c57c4d57e
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-05-19 03:26:36 -0400

    Add dots, next, and previous functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
 import "./styles.css";
+
+let index = 0;
+const carouselImages = document.querySelectorAll(".carousel-image");
+const carouselDots = document.querySelectorAll(".carousel-dot");
+const nextButton = document.querySelector("#next-button");
+const previousButton = document.querySelector("#previous-button");
+
+const showImage = function (indexReference) {
+  carouselImages.forEach((image, imageNumber) => {
+    if (imageNumber === indexReference) {
+      image.style.display = "block";
+    } else {
+      image.style.display = "none";
+    }
+  });
+
+  carouselDots.forEach((dot, imageNumber) => {
+    if (imageNumber === indexReference) {
+      dot.style.backgroundColor = "rgb(0,0,0)";
+    } else {
+      dot.style.backgroundColor = "rgb(255,255,255)";
+    }
+  });
+};
+
+carouselDots.forEach((dot, imageNumber) => {
+    dot.addEventListener("click", () => {
+        index = imageNumber;
+        showImage(index);
+    })
+})
+
+nextButton.addEventListener("click", () => {
+  if (index === carouselImages.length - 1) {
+    index = 0;
+  } else {
+    index += 1;
+  }
+
+  showImage(index);
+});
+
+previousButton.addEventListener("click", () => {
+  if (index === 0) {
+    index = carouselImages.length - 1;
+  } else {
+    index -= 1;
+  }
+
+  showImage(index);
+});
+
+showImage(index);
